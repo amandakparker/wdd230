5f20095ff14476762df2fb83a7cd919312382600
amandakparker <foroneangelgirl@byui.edu>
2022-10-29 21:59:40 -0400

    hamburger working

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -11,5 +11,5 @@ hambutton.addEventListener(
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {
-  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+  if (window.innerWidth > 1024) mainnav.classList.remove("responsive");
 };
