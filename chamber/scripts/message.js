fdd5ec2c5766994960c92f4db17bc3841067034e
amandakparker <foroneangelgirl@byui.edu>
2022-11-05 23:00:52 -0400

    updated message.js file

--- chamber/scripts/message.js
+++ chamber/scripts/message.js
@@ -2,9 +2,7 @@ const alertmsg = document.querySelector(".message");
 var currentDate = new Date();
 var day = currentDate.getDay();
 
-if (day == 1) {
-  alertmsg.classList.toggle("show");
-} else if (day == 2) {
+if (day == 1 || day == 2) {
   alertmsg.classList.toggle("show");
 } else {
   alertmsg.classList.toggle("hide");
